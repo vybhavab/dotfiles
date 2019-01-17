b7be4bbbc3420544168433b706f6adedcda014b6
vybhavb <vybhavbugs+github@gmail.com>
2019-01-17 01:33:52 -0800

    Adding all the dotfiles for i3, vim, zsh, and git

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# vim
+ln -s ${BASEDIR}/vimrc ~/.vimrc
+
+# zsh
+ln -s ${BASEDIR}/zshrc ~/.zshrc
+
+# git
+ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+
+#hyper
+ln -s ${BASEDIR}/hyper.js ~/.hyper.js
+
+#i3
+ln -s ${BASEDIR}/config/i3/config ~/.config/i3/config

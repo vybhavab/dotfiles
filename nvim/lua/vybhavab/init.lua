c2b22059e5c6021c6776bf4a057324e152ed6d72
Vybhav Achar Bhargav <vybhavab@gmail.com>
2024-02-26 19:42:28 -0800

    feat: add ts-error-translator

--- nvim/lua/vybhavab/init.lua
+++ nvim/lua/vybhavab/init.lua
@@ -39,6 +39,8 @@ autocmd('LspAttach', {
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
     augroup("__formatter__", { clear = true })
--- nvim/lua/vybhavab/lazy/init.lua
+++ nvim/lua/vybhavab/lazy/init.lua
@@ -10,6 +10,12 @@ return {
       vim.keymap.set("n", "<F9>", ":UndotreeToggle<CR>")
     end
   },
+  {
+    'dmmulroy/ts-error-translator.nvim',
+    config = function()
+      require('ts-error-translator').setup()
+    end
+  },
   'sbdchd/neoformat',
   'nvim-lua/plenary.nvim',
 }
